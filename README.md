2c04f9c4e0b59830b781a6ca4b736ebc054cae22
yashuwantjohn <yashuwant.john.s69@kalvium.community>
2025-04-24 12:30:12 +0530

    commited

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # backend-CA1
-deployment link : # backend-CA1
+deployment link : https://backend-ca1-m6r0.onrender.com
--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.get('/',(req ,res) => {
-     res.send('welcome to signup page!');
+     return res.send('welcome to signup page!');
 })
 app.post("/signup",(req,res) => {
     const{email,password,}= req.body;
 
     if(!email){
-        res.status(400).send("email cannot be empty");
+        return res.status(400).send("email cannot be empty");
     }
     if(!password.length>8 && password.length<=16){
-        res.status(400).send("password length should be greater than 8 and less than or equal to 16 and password cannot be empty");
+        return res.status(400).send("password length should be greater than 8 and less than or equal to 16 and password cannot be empty");
     }
-    res.send("signup successful");
+    return res.send("signup successful");
 })
 app.listen(port,() =>{
     console.log(`server is listening at http://localhost:${port}`);
